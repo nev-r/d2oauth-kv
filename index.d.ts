d574dded8cfda8d4ff7758d0efff82f1f864d97e
nev <gh@f-m.fm>
2021-12-04 07:44:30 -0800

    .

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+import { BungieNetToken } from "d2oauth-base";
 import { HttpClient } from "bungie-api-ts/http";
 import "cross-fetch/dist/node-polyfill.js";
 export declare function createOauthHttpClientKV(apiKey: string, client_id: string, client_secret: string, kvFileLocation?: string, options?: {
@@ -28,3 +29,8 @@ export declare function createOauthHttpClientKV(apiKey: string, client_id: strin
     verbose?: boolean;
 }): HttpClient;
 export declare function setupTokenWithAuthCode(authorization_code: string, client_id: string, client_secret: string, kvFileLocation?: string): Promise<void>;
+export declare function injectExistingBungieNetToken(
+/** a valid existing token, which we'll now start refreshing through this instance */
+token: BungieNetToken, kvFileLocation?: string, 
+/** force this to be written into token storage as the new token, even if one already exists */
+forceOverwrite?: boolean): Promise<void>;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Keyv from "keyv";
 import { KeyvFile } from "keyv-file";
-import { createOauthHttpClient, setupTokenWithAuthCode as stwac, } from "d2oauth-base";
+import { createOauthHttpClient, looksLikeBnetAuthToken, setupTokenWithAuthCode as stwac, } from "d2oauth-base";
 import "cross-fetch/dist/node-polyfill.js";
 export function createOauthHttpClientKV(apiKey, client_id, client_secret, kvFileLocation = `./db/d2oauth.json`, options = {}) {
     const oauthStore = new Keyv({
@@ -16,5 +16,29 @@ export async function setupTokenWithAuthCode(authorization_code, client_id, clie
     });
     const oauthStore = new Keyv({ store });
     await stwac(authorization_code, client_id, client_secret, (tm) => oauthStore.set("token", tm));
+    await store.saveToDisk();
+}
+export async function injectExistingBungieNetToken(
+/** a valid existing token, which we'll now start refreshing through this instance */
+token, kvFileLocation = `./db/d2oauth.json`, 
+/** force this to be written into token storage as the new token, even if one already exists */
+forceOverwrite) {
+    const store = new KeyvFile({
+        filename: kvFileLocation,
+    });
+    const oauthStore = new Keyv({ store });
+    if (!looksLikeBnetAuthToken(token)) {
+        console.error(token);
+        throw `this doesn't look like a token`;
+    }
+    const tokenMeta = {
+        token,
+        expires_at: 0,
+        refresh_expires_at: Date.now() + token.refresh_expires_in * 1000,
+    };
+    // bail if there's already a token and overwrite isn't set
+    if ((await oauthStore.get("tokenMeta")) && !forceOverwrite)
+        return;
+    await oauthStore.set("tokenMeta", tokenMeta);
     await store.saveToDisk();
 }
